5e271625fb0c28bae49d10b1c4760ade498189ba
sampolz <68559864+sampolz@users.noreply.github.com>
2026-03-31 22:50:44 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,17 +19,24 @@ export default function Home() {
         <p className="text-sm leading-7 mb-4">
           I'm a senior CS major at{" "}
           <a href="https://colby.edu/" className="underline underline-offset-4">Colby College</a>
-          , passionate about architecture, sculpture, and cool tech
+          , passionate about architecture, sculpture, and education. At school, I am a TA and Tutor for the CS department, and president of the Coding Club 
+          - a program where we go to the local high school and help out in CS classes.
         </p>
         <p className="text-sm leading-7 mb-4">
-          I spent this past summer interning at Apple and recieved a full-time return offer, but am looking for something new in the NYC or Boston area.
+          I spent this past summer interning at <a href="https://apple.com/" className="underline underline-offset-4">Apple </a>
+          as a Software Engineer and recieved a full-time return offer, but am <a className="font-semibold">looking for something new in the NYC or Boston area.</a>
+        </p>
+        <p className="text-sm leading-7 mb-4">
+          Prior to Apple, I spent 2 summers interning at <a href="https://teradyne.com/" className="underline underline-offset-4">Teradyne,</a> also as a Software Engineer. 
+          In high school, I spent 3 summers interning in business development at <a href="https://www.legendsoflearning.com/" className="underline underline-offset-4">Legends of Learning,</a> 
+          a startup building a platform of educational games for K-8 students.
         </p>
         <p className="text-sm leading-7">
-          Feel free to reach out at{" "}
+          Feel free to reach out to discuss potential opportunities, or just to chat at{" "}
           <a href="sam.polyakov@gmail.com" className="underline underline-offset-4">
             sam.polyakov [at] gmail [dot] com
           </a>
-          .
+          !
         </p>
       </section>
 

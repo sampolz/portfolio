51acb021d8ec948c4560debc93e36c1f0597865f
sampolz <68559864+sampolz@users.noreply.github.com>
2026-04-02 19:53:14 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,8 @@ export default function Home() {
         <p className="text-sm leading-7 mb-4">
           I spent this past summer interning at <a href="https://apple.com/" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4">Apple</a>{" "}
           as a Software Engineer working on AppleConnect (Apple's internal SSO portal & VPN) and recieved a full-time return offer. 
-          <a className="font-bold"> Today, I am looking for new opportunities in NYC or Boston.</a>
+          <a className="font-bold"> Today, I am looking for new opportunities in NYC or Boston.</a> 
+          I work primarily in Python, C#, Swift, and Java.
         </p>
         <p className="text-sm leading-7 mb-4">
           Prior to Apple, I spent 2 summers interning at <a href="https://teradyne.com/" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4">Teradyne</a>

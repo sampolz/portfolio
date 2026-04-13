76858aa7e4b588f2a5befa13038bb9bfaa28dc8d
sampolz <68559864+sampolz@users.noreply.github.com>
2026-04-13 01:13:19 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,8 @@ export default function Home() {
           , a startup building a platform of educational games for K-8 students.
         </p>
         <p className="text-sm leading-7 mb-4">
-          I'm also planning a trip to Banff National Park with my girlfriend. Check out the{" "}
-          <a href="/banff-itinerary" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4">intinerary</a>!
+          I'm also planning a trip to Banff National Park in a campervan with my girlfriend! Check out the{" "}
+          <a href="/banff-itinerary" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4">intinerary</a> 😁.
         </p>
         <p className="text-sm leading-7">
           Feel free to reach out at{" "}
